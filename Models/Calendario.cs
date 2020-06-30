bc7b4cca59191feef9c3b3c93fc76af8fd58a4c8
Daniel Di Lorenzo <ddilorenzo@gmail.com>
2020-06-30 12:10:25 -0300

    tabla calendario

--- Models/Calendario.cs
+++ Models/Calendario.cs
@@ -5,12 +5,13 @@ namespace MascotasApi.Models
     {
         public int Id { get; set; }
         public int MascotasTipoId { get; set; }
-        public string Descripcion { get; set; }
+        public int VacunasId { get; set; }
         public string Enfermedades { get; set; }
+        public string Edad { get; set; }
         public bool Optativa { get; set; }
-        public bool Cachorro { get; set; }
         public bool Activo { get; set; }
         public MascotasTipo MascotasTipo { get; set; }
+        public Vacunas Vacunas { get; set; }
 
     }
 }
