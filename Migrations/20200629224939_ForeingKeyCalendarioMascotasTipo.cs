d5f7e0b3376f325a83f60a0b2df6fb37c4db3959
mdominguez33 <mdominguez33@hotmail.com>
2020-06-29 22:51:18 -0300

    Agregar Relacion Calendario-MascotasTipo en SQL

---
+++ Migrations/20200629224939_ForeingKeyCalendarioMascotasTipo.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MascotasApi.Migrations
+{
+    public partial class ForeingKeyCalendarioMascotasTipo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Calendario_MascotasTipoId",
+                table: "Calendario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calendario_MascotasTipoId",
+                table: "Calendario",
+                column: "MascotasTipoId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Calendario_MascotasTipoId",
+                table: "Calendario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calendario_MascotasTipoId",
+                table: "Calendario",
+                column: "MascotasTipoId");
+        }
+    }
+}
